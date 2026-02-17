f3178400dbbd7278c8ddfaa5ddbeb8958e19b707
Roshan <roshandnikam3158@gmail.com>
2026-02-17 14:20:14 +0530

    added script to start and build the project

--- 01_Inception/app.js
+++ 01_Inception/app.js
@@ -1,34 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
 
-
-const parent = React.createElement(
-    "div", 
-    { id: "parent" },
-    [ React.createElement(
-        "div", 
-        { id: "child" }, 
-        [React.createElement("h1", {id: "heading" }, "This is nested heading") 
-        ,React.createElement("h2", {id: "second_heading"}, "This is second heading")]    
-        ),
-         React.createElement(
-        "div", 
-        { id: "child2" }, 
-        [React.createElement("h1", {id: "heading2" }, "This is nested heading") 
-        ,React.createElement("h2", {id: "second_heading2"}, "This is second heading")]    
-        )
-
-    ]
-   
-    );
-
-
-const root = createRoot(document.getElementById("root"));
-root.render(parent);
-
-
-
-
-
- 
+    
