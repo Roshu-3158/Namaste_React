71bb9812a99855e7bf4e4d49155ef15bd884c9a6
Roshan <roshandnikam3158@gmail.com>
2026-02-18 17:36:28 +0530

    Lecture 04 - Building App

--- 01_Inception/app.js
+++ 01_Inception/app.js
@@ -2,36 +2,34 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import { jsx } from "react/jsx-runtime";
 
-// jsx (transpiled before it reaches the js ) -  Parcel -  babel
-// jsx => babel transpiles it to React.createElement => Reactelement => js-Object => HtmlElement(render)
 
-// react Element
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is react element",
-);
 
-// jsx
-const jsxheading = <h1>Namste react using jsx </h1>;
+const Header = () => {
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://marketplace.canva.com/EAGZJ2Na9V0/1/0/1600w/canva-red-and-orange-bold-fast-food-restaurant-animated-logo-0ujmrXVcncU.jpg"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const Title = () => (
-  <h1 className="head" tabIndex="5">
-    Namste react using jsx
-  </h1>
-);
 
-// Component Composition 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title/>
-    <h1 className="heading">Namste react functional component</h1> 
+const Applayout = () => {
+  return(
+  <div className = "app">
+    <Header/>
   </div>
-);
-
-// const HeadingComponet2 = () => <h1>Namste react functional component</h1>;
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(jsxheading);
-
-root.render(<HeadingComponent/>);
+root.render(<Applayout/>);
